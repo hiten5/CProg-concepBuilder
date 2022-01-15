9da414c4b955d01b20671734d5953c03733476f4
hiten5 <iatsimba@gmail.com>
2022-01-15 13:32:57 +0530

    Basic python programs

---
+++ 7.2.conditional_statememnt_if_else.py
@@ -0,0 +1,7 @@
+usr_ip=int(input('enter any number'))
+if usr_ip<10:
+    print('x is smaller then 10')                  #notice indentation, it is under I if statement
+else:
+    print('x is greater than or equal to10')       #the same indentation blocks are under else statement
+    
+print('thank you')
