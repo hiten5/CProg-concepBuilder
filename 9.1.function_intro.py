7fff5f505b208e304fd7a79720c5de9b674ae654
hiten5 <iatsimba@gmail.com>
2022-01-16 17:52:01 +0530

    Basic python programs

---
+++ 9.1.function_intro.py
@@ -0,0 +1,6 @@
+def thing():                #def is keyword to define new function named thing
+    print('hello')          #the function named thing is saved in memory
+    print('fun')            #this code block is for naming and defining function, so its job is to save function and here there is nothing printed on screen
+
+thing()                      #thing() function is called, now the code in thing is executes
+print('function example')
