32360d69d29397da71e2c852693bf574400ad985
hiten5 <iatsimba@gmail.com>
2022-01-16 18:36:33 +0530

    Basic python programs

---
+++ 9.3.function_parameter2.py
@@ -0,0 +1,8 @@
+def add(x,y):                   #function add have two arguments
+    result=int(x)+int(y)
+    return result
+
+usr_ip1=input('enter I number')
+usr_ip2=input('enter II number')
+
+print(add(usr_ip1,usr_ip2))     #passing two parameters to function add
